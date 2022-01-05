83a093d43959ef44862b58983368c95f91a6cb62
Lnnnnn <61160282@go.buu.ac.th>
2022-01-05 11:46:41 +0700

    Create and Inital Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, {useState, Component} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './App.css';
+import { Icon } from "leaflet";
+import axios from "axios";
+
+export default class App extends Component {
+  // const data = 
+
+  render() {
+    console.log("Complete!!")
+    return (
+      <div>
+         <MapContainer center={[13.2848334, 100.9179506]} zoom={14} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[13.281353, 100.927505]}>
+            <Popup>
+              
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    )
+  }
+}
