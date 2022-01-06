30330bb2fc18faff9b1608a1b9af65e288b59241
Lnnnnn <61160282@go.buu.ac.th>
2022-01-06 14:00:04 +0700

    Show marker from db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,43 @@
-import React, {useState, Component} from "react";
+import React, {useState, useEffect, Component} from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
 import { Icon } from "leaflet";
 import axios from "axios";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-         <MapContainer center={[13.2848334, 100.9179506]} zoom={14} scrollWheelZoom={true}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[13.281353, 100.927505]}></Marker>
-        </MapContainer>
-      </div>
-    )
-  }
+const client = axios.create({
+  baseURL: "api/"
+})
+export default function App() {
+  const [bin, setBin] = useState(null);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    async function getBin() {
+      await client.get('bin/')
+      .then((response) => {
+        setBin(response.data);
+      })
+      .catch(error => {
+        setError(error);
+      });
+    }
+    getBin();
+  }, []);
+  
+  if (error) return `Error : ${error.message}`;
+  if (!bin) return null;
+
+  return (
+    <div className="container-fluid px-0">
+      <MapContainer center={[13.2848334, 100.9179506]} zoom={14} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {bin.map(bin => (
+          <Marker key={bin.id} position={[bin.lat, bin.lng]}></Marker>
+        ))}
+      </MapContainer>
+    </div>
+  )
 }
