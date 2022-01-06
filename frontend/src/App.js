1dc5e7cf69fb0729217ded11d5664b17a37c5c44
61160182 <61160182@go.buu.ac.th>
2022-01-06 20:25:27 +0700

    button Add data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,11 @@ import axios from "axios";
 const client = axios.create({
   baseURL: "api/"
 })
+
 export default function App() {
   const [bin, setBin] = useState(null);
   const [error, setError] = useState(null);
-  
+ 
   useEffect(() => {
     async function getBin() {
       await client.get('bin/')
@@ -29,7 +30,7 @@ export default function App() {
 
   return (
     <div className="container-fluid px-0">
-      <MapContainer center={[13.2848334, 100.9179506]} zoom={14} scrollWheelZoom={true}>
+       <MapContainer center={[13.2848334, 100.9179506]} zoom={14} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -38,6 +39,65 @@ export default function App() {
           <Marker key={bin.id} position={[bin.lat, bin.lng]}></Marker>
         ))}
       </MapContainer>
-    </div>
+      
+      <div className="button-insert">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+          AddBin
+        </button>
+      </div>
+      <div class="modal fade bd-example-modal-xl" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog-centered modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Rubbish bin location</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="container">
+                  <form>
+                    <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="inputLatitude">Latitude</label>
+                        <input type="latitude" class="form-control" id="inputLatitude" />
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="inputLongitude">Longitude</label>
+                        <input type="longitude" class="form-control" id="inputLongitude" />
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="inputLocation">Location</label>
+                      <input type="text" class="form-control" id="inputLocation" />
+                    </div>
+                    <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="inputType">Type</label>
+                        <input type="type" class="form-control" id="inputType" />
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="inputTag">Tag</label>
+                        <select id="inputTag" class="form-control">
+                          <option selected>Choose...</option>
+                          <option>...</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <DatePicker />
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-light">Cancel</button>
+                <button type="button" class="btn btn-secondary">Clear</button>
+                <button type="button" class="btn btn-success">Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>//container
   )
 }
