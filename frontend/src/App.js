31eeb8c556ff7fe221dc3fa50fbe439fcd2bff4a
Lnnnnn <61160282@go.buu.ac.th>
2022-01-06 21:30:22 +0700

    Add date&time input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,11 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
 import { Icon } from "leaflet";
 import axios from "axios";
+import DatePicker, { registerLocale, setDefaultLocale} from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import ptBR from 'date-fns/locale/pt-BR';
+registerLocale('pt-BR', ptBR)
 
 const client = axios.create({
   baseURL: "api/"
@@ -11,7 +16,8 @@ const client = axios.create({
 export default function App() {
   const [bin, setBin] = useState(null);
   const [error, setError] = useState(null);
- 
+  const [startDate, setStartDate] = useState(new Date());
+
   useEffect(() => {
     async function getBin() {
       await client.get('bin/')
@@ -41,59 +47,75 @@ export default function App() {
       </MapContainer>
       
       <div className="button-insert">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
           AddBin
         </button>
       </div>
-      <div class="modal fade bd-example-modal-xl" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog-centered modal-dialog modal-xl" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Rubbish bin location</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <div className="modal fade bd-example-modal-xl" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div className="modal-dialog-centered modal-dialog modal-xl" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLongTitle">Rubbish bin location</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div class="modal-body">
-                <div class="container">
+              <div className="modal-body">
+                <div className="container">
                   <form>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputLatitude">Latitude</label>
-                        <input type="latitude" class="form-control" id="inputLatitude" />
+                    <div className="form-row">
+                      <div className="form-group col-md-6">
+                        <label htmlFor="inputLatitude">Latitude</label>
+                        <input type="latitude" className="form-control" id="inputLatitude" />
                       </div>
-                      <div class="form-group col-md-6">
-                        <label for="inputLongitude">Longitude</label>
-                        <input type="longitude" class="form-control" id="inputLongitude" />
+                      <div className="form-group col-md-6">
+                        <label htmlFor="inputLongitude">Longitude</label>
+                        <input type="longitude" className="form-control" id="inputLongitude" />
                       </div>
                     </div>
-                    <div class="form-group">
-                      <label for="inputLocation">Location</label>
-                      <input type="text" class="form-control" id="inputLocation" />
+                    <div className="form-group">
+                      <label htmlFor="inputLocation">Location</label>
+                      <input type="text" className="form-control" id="inputLocation" />
                     </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputType">Type</label>
-                        <input type="type" class="form-control" id="inputType" />
+                    <div className="form-row">
+                      <div className="form-group col-md-6">
+                        <label htmlFor="inputType">Date</label>
+                        <DatePicker className="form-control" selected={startDate} onChange={(date) => setStartDate(date)} />
                       </div>
-                      <div class="form-group col-md-6">
-                        <label for="inputTag">Tag</label>
-                        <select id="inputTag" class="form-control">
+                      <div className="form-group col-md-6">
+                        <label htmlFor="inputType">Time</label>
+                        <DatePicker
+                          className="form-control"
+                          selected={startDate}
+                          onChange={(date) => setStartDate(date)}
+                          locale="pt-BR"
+                          showTimeSelect
+                          showTimeSelectOnly
+                          timeCaption="Time"
+                          dateFormat="pp"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="form-group col-md-6">
+                        <label htmlFor="inputType">Type</label>
+                        <input type="type" className="form-control" id="inputType" />
+                      </div>
+                      <div className="form-group col-md-6">
+                        <label htmlFor="inputTag">Tag</label>
+                        <select id="inputTag" className="form-control">
                           <option selected>Choose...</option>
                           <option>...</option>
                         </select>
                       </div>
                     </div>
-                    <div class="form-group col-md-6">
-                      <DatePicker />
-                    </div>
                   </form>
                 </div>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-light">Cancel</button>
-                <button type="button" class="btn btn-secondary">Clear</button>
-                <button type="button" class="btn btn-success">Save</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-light">Cancel</button>
+                <button type="button" className="btn btn-secondary">Clear</button>
+                <button type="button" className="btn btn-success">Save</button>
               </div>
             </div>
           </div>
