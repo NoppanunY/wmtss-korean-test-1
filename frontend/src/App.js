a90fcf82a5b8178c638047321b527a277ad9ad28
Lnnnnn <61160282@go.buu.ac.th>
2022-01-10 12:28:38 +0700

    Add create bin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,165 +1,68 @@
 import React, {useState, useEffect, Component} from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import './App.css';
+import { MapContainer, TileLayer, Marker, Popup, ZoomControl} from 'react-leaflet';
 import { Icon } from "leaflet";
 import axios from "axios";
-import DatePicker, { registerLocale, setDefaultLocale} from "react-datepicker";
-import Moment from 'moment';
-import "react-datepicker/dist/react-datepicker.css";
-import ptBR from 'date-fns/locale/pt-BR';
+import './App.css';
 
-registerLocale('pt-BR', ptBR)
+import { Button, Collapse, Card } from 'react-bootstrap';
+import { ModalForm } from './components/modal-form/App';
 
 export default function App() {
-
-  const [bin, setBin] = useState(null);
+  const [bins, setBins] = useState([]);
   const [error, setError] = useState(null);
-  const [tag, setTag] = useState(null);
+  const [criteria, setCriteria] = useState({});
 
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [location, setLocation] = useState("");
-  const [type, setType] = useState("");
-  const [date, setDate] = useState(new Date());
-  const [time, setTime] = useState(new Date());
-  const [description, setDescription] = useState("");
-  const [tagSelected, setTagSelected] = useState(null);
+  const [showModalForm, setShowModalForm] = useState(false);
 
   useEffect(() => {
-    async function getBin() {
+    async function getBins() {
       await axios.get('api/bin/')
       .then((response) => {
-        setBin(response.data);
+        setBins(response.data);
       })
       .catch(error => {
         setError(error);
       });
     }
-    async function getTag(){
-      await axios.get('api/tag/')
-      .then((response) => {
-        setTag(response.data);
-        setTagSelected(response.data[0].id);
-      })
-      .catch(error => {
-        setError(error);
-      })
-    }
-    getBin();
-    getTag();
-  }, []);
+    getBins();
+  }, [showModalForm]);
   
-  function insertBin(){
-    console.log(tagSelected);
-    axios.post('api/bin/',{
-      "lat": lat,
-      "lng": lng,
-      "location": location,
-      "type": type,
-      "date": Moment(date).format('YYYY-DD-MM'),
-      "time": Moment(time).format('hh:mm:ss'),
-      "description": description,
-      "tag": tagSelected
-    })
-    .then(() => {
-      console.log("Success!");
-    })
+  const handleModalOpen = () => {
+    console.log("Open!")
+    setShowModalForm(true)
+  }
+
+  const hideModal = e => {
+    setShowModalForm(false)
+  }
+
+  const handleOk = () =>{
+    setShowModalForm(false)
   }
 
   if (error) return `Error : ${error.message}`;
-  if (!bin || !tag) return null;
 
   return (
     <div className="container-fluid px-0">
-       <MapContainer center={[13.2848334, 100.9179506]} zoom={14} scrollWheelZoom={true}>
+       <MapContainer center={[13.2848334, 100.9179506]} zoom={14} scrollWheelZoom={true} zoomControl={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {bin.map(bin => (
-          <Marker key={bin.id} position={[bin.lat, bin.lng]}></Marker>
+        {bins.map(bin => (
+          <Marker key={bin.id} position={[bin.lat, bin.lng]} 
+            eventHandlers={{
+              click: (e) => {
+                console.log('marker clicked', bin)
+                
+              },
+            }}
+          ></Marker>
         ))}
       </MapContainer>
       
-      <div className="button-insert">
-        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-          Add Bin
-        </button>
-      </div>
-      <div className="modal fade bd-example-modal-xl" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div className="modal-dialog-centered modal-dialog modal-xl" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLongTitle">Rubbish bin location</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <div className="container">
-                  <form>
-                    <div className="form-row">
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputLatitude">Latitude</label>
-                        <input type="latitude" className="form-control" id="inputLatitude" onChange={(event) => { setLat(event.target.value)}}/>
-                      </div>
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputLongitude">Longitude</label>
-                        <input type="longitude" className="form-control" id="inputLongitude" onChange={(event) => { setLng(event.target.value)}}/>
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="inputLocation">Location</label>
-                      <input type="text" className="form-control" id="inputLocation" onChange={(event) => { setLocation(event.target.value)}}/>
-                    </div>
-                    <div className="form-row">
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputType">Date</label>
-                        <DatePicker className="form-control" selected={date} onChange={(date) => setDate(date)}/>
-                      </div>
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputType">Time</label>
-                        <DatePicker
-                          className="form-control"
-                          selected={time}
-                          onChange={(date) => setTime(date)}
-                          locale="pt-BR"
-                          showTimeSelect
-                          showTimeSelectOnly
-                          timeCaption="Time"
-                          dateFormat="pp"
-                        />
-                      </div>
-                    </div>
-                    <div className="form-row">
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputType">Type</label>
-                        <input type="type" className="form-control" id="inputType" onChange={(event) => { setType(event.target.value)}}/>
-                      </div>
-                      <div className="form-group col-md-6">
-                        <label htmlFor="inputTag">Tag</label>
-                        <select id="inputTag" className="form-control" onChange={(event) => {setTagSelected(event.target.value)}}>
-                          {tag.map(tag => (
-                            <option key={tag.id} value={tag.id}>{tag.name}</option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="inputLocation">Description</label>
-                      <input type="text" className="form-control" id="inputLocation" onChange={(event) => { setDescription(event.target.value)}}/>
-                    </div>
-                  </form>
-                </div>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-light">Cancel</button>
-                <button type="button" className="btn btn-secondary">Clear</button>
-                <button type="button" className="btn btn-success" onClick={insertBin}>Save</button>
-              </div>
-            </div>
-          </div>
-        </div>
+      <ModalForm show={showModalForm} handleClose={hideModal}/>
+      <Button className="button-insert" variant="primary" onClick={handleModalOpen}>Add Bin</Button>
     </div>//container
   )
 }
