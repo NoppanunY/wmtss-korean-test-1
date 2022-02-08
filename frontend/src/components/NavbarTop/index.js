fb6dec50b79c0a1e565d2b71f6d64ecb2d8efae1
Lnnnnn <61160282@go.buu.ac.th>
2022-02-09 00:52:13 +0700

    Accept incoming

--- frontend/src/components/NavbarTop/index.js
+++ frontend/src/components/NavbarTop/index.js
@@ -4,35 +4,6 @@ import "./style.css";
 const NavbarTop = props => {
     return (
         <div>
-<<<<<<< HEAD
-            <nav className="navbar navbar-light bg-light" id="nav1">
-                <div className="row">
-                    <form className="form-inline">
-                    <div className="col-1"> 
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                    </div>                  
-                    <div className="col-4">
-                        {/* <form className="form-inline"> */}
-                            <div className="input-group" id="inputSearch">
-                                <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" id="inputSrarch"/>
-                                <div className="input-group-append">
-                                    <button className="btn btn-outline-secondary" type="button" id="iconSearch"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>
-                                    </button>
-                                </div>
-                            </div>
-                        {/* </form> */}
-                    </div>
-                    
-                    <div className="col"><button className="btn btn-light" type="button" id="btnNav">Groceries</button></div>          
-                    <div className="col"><button className="btn btn-light" type="button" id="btnNav">Restaurants</button></div>
-                    <div className="col"><button className="btn btn-light" type="button" id="btnNav">Hotels</button></div>
-                    <div className="col"><button className="btn btn-light" type="button" id="btnNav">Coffee</button></div>
-                    
-                    </form>
-=======
             <div class="row">
             <div class="col-sm-1"> 
                     <button className="btn btn-secondary1" type="button">
@@ -78,7 +49,6 @@ const NavbarTop = props => {
                 </div>
                 <div class="col-5"></div>
                 
->>>>>>> 11c06840f3546957c9d22bd08e41ad3d898f34f5
                 </div> 
                 {/* layout */}
             
