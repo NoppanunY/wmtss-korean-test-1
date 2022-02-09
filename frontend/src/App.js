b4edfe97bddfa418370b4265d2c6356576703f80
61160182 <61160182@go.buu.ac.th>
2022-02-09 21:38:39 +0700

    1. Make sideBar 2. edit button  Add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Modal from './components/Modal';
 import CreateBin from './components/CreateBin';
 import UpdateBin from './components/UpdateBin';
 import SideBar from './components/SideBar';
+import SideBarMenu from './components/SideBarMenu';
 import NavbarTop from './components/NavbarTop';
 
 import {
@@ -137,13 +138,11 @@ export default function App() {
     <div className="app">
       <NavbarTop/>
 
-      { activeSidebar && (
-        <SideBar 
+        <SideBarMenu 
           criteria={criteria}
           hideSidebar={() => {setActiveSidebar(false)}}
           setModal={setModal}
-        ></SideBar>
-      )}
+        ></SideBarMenu>
 
       <MapContainer center={[13.2848334, 100.9179506]} zoom={14} scrollWheelZoom={true} zoomControl={false}>
         <TileLayer
@@ -185,7 +184,10 @@ export default function App() {
       </Button> */}
 
       <Button type="button" id="btnAdd" onClick={() => {setModal({name: "Rubbish bin location ", type: "ADD_BIN"})}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-trash3" viewBox="0 0 16 16">
+          <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" className="bi bi-plus-circle-fill" viewBox="0 0 16 16" id="iconMenuBin2">
           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"></path>
         </svg>
       </Button>
