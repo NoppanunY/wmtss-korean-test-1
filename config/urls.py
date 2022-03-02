41221b7b1773bbda781a106a844c7f8528a490e4
Lnnnnn <rooslanza@gmail.com>
2022-03-02 13:43:32 +0700

    Add re_path

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from model.apps.bin import views as binModel
 from model.apps.image import views as imageModel
 from model.apps.tag import views as tagModel
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 
@@ -14,5 +15,6 @@ router.register(r'tag', tagModel.TagView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/auth/', include('model.common.base.api.urls'))
+    path('api/auth/', include('model.common.base.api.urls')),
+    re_path('.*',TemplateView.as_view(template_name='index.html'))
 ]
